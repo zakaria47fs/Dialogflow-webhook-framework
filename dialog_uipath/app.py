345fc2aeb77e0ba43175d0673d849f1b66986b6e
zakaria47fs <55286921+zakaria47fs@users.noreply.github.com>
2021-11-15 20:39:57 +0400

    project to aws lambda sam

---
+++ dialog_uipath/app.py
@@ -0,0 +1,61 @@
+'''
+This script requires "requests": http://docs.python-requests.org/
+To install: pip install requests
+
+client_id = "XXXXXXXXXXXXXX"
+refresh_token = "XXXXXXXXXXXXXXXXXXC" #User Key
+tenant_name = "XXXXXX"
+account_logical_name = "XXXXX"
+process_name = "Demo Process"
+'''
+
+import json
+from flask_lambda import FlaskLambda
+from flask import request
+from start_process import default_start_process
+import json
+
+
+app = FlaskLambda(__name__)
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'Test correct'
+
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+
+    req = request.get_json(force=True)
+    query_result = req.get('queryResult')
+
+    client_id = request.headers.get('client_id')
+    refresh_token = request.headers.get('refresh_token') #User Key
+    tenant_name = request.headers.get('tenant_name')
+    account_logical_name = request.headers.get('account_logical_name')
+    process_name = request.headers.get('process_name')
+    param_bool = request.headers.get('param_bool')
+    param_bool = bool( str(param_bool).lower() == 'true')
+
+    chatbot_parameters = query_result.get('parameters')
+
+    process_response = default_start_process(client_id, refresh_token, tenant_name,\
+                        account_logical_name, process_name, param_bool, chatbot_parameters)
+    process_response = process_response.get('value')[0].get('OutputArguments')
+    
+    if process_response: process_response = json.loads(process_response).get('chatbot_response')
+
+    print(process_response)
+    
+    fulfillment_text = ''
+    #if query_result.get('action')=='create_event':
+    #    event_name = query_result.get('parameters').get('event_name')
+    #fulfillment_text = process_response
+
+    response = {
+                "fulfillmentText": fulfillment_text,
+                "source": "webhookdata"
+               }
+    
+    return response
